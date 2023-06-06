64cd274c8e42ee59ea53d563e43c70a80518d936
Michael Elfassy <mike.elfassy@gmail.com>
2023-06-05 22:57:13 -0400

    commit tree 4d681c07ae194c6f7e43b8555a8781d7e1ee208b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_reader :anagram
+    def initialize(anagram)
+        @anagram = anagram
+    end
+
+    def match(words)
+        matches = []
+        sorted_anagram = @anagram.chars.sort
+        words.each do |word|
+            if word.chars.sort == sorted_anagram
+                matches << word
+            end
+        end
+        matches
+    end
+end
